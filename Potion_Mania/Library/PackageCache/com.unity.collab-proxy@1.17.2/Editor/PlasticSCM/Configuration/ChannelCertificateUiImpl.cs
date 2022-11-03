e942ef182af9d9f635a47423898f9f5c8255c061
tngo17 <95452268+tngo17@users.noreply.github.com>
2022-11-03 17:24:27 -0400

    Delete Potion_Mania/Library directory

--- Potion_Mania/Library/PackageCache/com.unity.collab-proxy@1.17.2/Editor/PlasticSCM/Configuration/ChannelCertificateUiImpl.cs
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:8821f24e9987792cbfd0e607c0518c300b93af71366ef841a64488a0221d1734
-size 3808
--- Potion_Mania/Library/PackageCache/com.unity.render-pipelines.universal@12.1.7/Runtime/Overrides/ColorLookup.cs
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:5ee1ddbdc06550000c89dc43c3177f28079f8bc862979cb28f6d214907608de4
-size 1654
