db0b0c624fee5d73861fad5da19629dda30ffeba
Tina N <tina17ngo@gmail.com>
2023-06-16 16:57:13 -0400

    clean up directories and trees

---
+++ javascript/javascript.js
@@ -0,0 +1,22 @@
+/*const startButton = document.getElementById("delayPlay");
+
+startButton.addEventListener("click", function() {
+  window.location.href = "./dif_load_screens/loadscreen.html"; // Replace "newpage.html" with the URL of the new page
+});
+*/
+
+
+const delayPlay = document.getElementById('delayPlay');
+const textElements = document.querySelectorAll("body *");
+
+delayPlay.addEventListener('click', function() {
+    setTimeout(function() {
+        textElements.forEach(function(text) {
+          text.classList.add("vanish");
+        });
+      }, 1000); 
+      
+    setTimeout(function() {
+        window.location.href = "../html/prologue.html"; // Replace with the URL of the new page
+      }, 5000);  
+});
